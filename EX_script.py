6e7b762b19cd5a9d626bbfc0b480cd48361c9f56
Jbalkan <jeff.balkanski@gmail.com>
2019-12-05 03:56:05 -0500

    saving each EX time series as seperate pkl files

--- EX_script.py
+++ EX_script.py
@@ -21,7 +21,7 @@ dir_path = os.path.join(current_dir, 'Aalborg_Jeff')
 cluster_dir_path = '/n/home03/lshaw/Tracking/Tracab/SuperLiga/'
 LEAGUE = 'DSL'
 PLAYER_ID_to_JERSEY_NUM_LOC = '../playerid_jerseynum_map.csv'
-OUTFILE_NAME = 'all_time_series_last_run.pkl'
+
 
 # helpers
 def get_time_series(players_full_game):
@@ -30,7 +30,6 @@ def get_time_series(players_full_game):
         player = row['obj']
         val_array = fatigue.get_energy_expenditure(player)
         metric_values.append([row['match_id'], row['player_id'], val_array])
-
     EX_all = np.array(metric_values)
     
     return EX_all
@@ -41,16 +40,18 @@ all_Aalborg_games = [x for x, _, _ in os.walk(dir_path) if x.count('_TracDAT')]
 all_cluster_games = [x for x, _, _ in os.walk(cluster_dir_path) if x.split('/')[-1].isnumeric()]
 all_cluster_games.sort(reverse=True)
 all_game_ids = list(map(int, [path.split('/')[-1] for path in all_cluster_games]))
+
+# player data
 game_ids_w_player_mapping = pd.read_csv(PLAYER_ID_to_JERSEY_NUM_LOC)['Match ID'].unique()
 print('Sucessfully read data paths and player data')
 
 # LONG RUN: for each game, get time series of energy expenditure
-games = all_cluster_games
-for x in games:
-    print(x)
+games = all_Aalborg_games
 for path in games:
-    # match_id, _ = path.replace(os.path.dirname(path) + '/', '').split('_') # local Aalborg
-    match_id = path.split('/')[-1] # cluster
+    game_data = {}
+    
+    match_id, _ = path.replace(os.path.dirname(path) + '/', '').split('_') # local Aalborg
+#     match_id = path.split('/')[-1] # cluster
     print('Starting energy expenditure calculcation for game {} at location {}'.format(match_id, path))
 
     # skip games which are not in player id mapping with player info
@@ -59,6 +60,7 @@ for path in games:
         continue
     
     # store path
+    game_data['data_path'] = path
     data_dict[match_id] = {'data_path': path}
     
     # read 
@@ -74,14 +76,17 @@ for path in games:
     time_series_mat = get_time_series(players_info)
     
     # store 
+    game_data['player_info'] = players_info.drop('obj', axis=1)
+    game_data['energy_x'] = time_series_mat
     data_dict[match_id]['player_info'] = players_info.drop('obj', axis=1)
     data_dict[match_id]['energy_x'] = time_series_mat
     
+    # save
+    OUTFILE_NAME = 'EX_time_series_game_{}.pkl'.format(match_id)
+    with open(os.path.join('../saved', OUTFILE_NAME), 'wb') as outfile:
+        pickle.dump(data_dict, outfile)
+        
     # free memory
     del team1_players, team0_players, match_tb, frames_tb
-    
-# save
-with open(os.path.join('../saved', OUTFILE_NAME), 'wb') as outfile:
-    pickle.dump(data_dict, outfile)
     
 print('successfully computed time series for all games')
